36cb481ddf053c92843a0c16b659c8d848eb3a33
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-30 11:46:15 +0300

    add speed variables

--- app/src/main/java/com/example/tripheo2410/galaxsee/RotatingNode.kt
+++ app/src/main/java/com/example/tripheo2410/galaxsee/RotatingNode.kt
@@ -1,2 +1,19 @@
 package com.example.tripheo2410.galaxsee
 
+import android.animation.ObjectAnimator
+import com.google.ar.sceneform.Node
+
+class RotatingNode(private val solarSettings: SolarSettings, private val isOrbit: Boolean) : Node() {
+    private var orbitAnimation: ObjectAnimator? = null
+    private var degreesPerSecond = 90.0f
+    private var lastSpeedMultiplier = 1.0f
+    private val animationDuration: Long
+        get() = (1000 * 360 / (degreesPerSecond * speedMultiplier)).toLong()
+    private val speedMultiplier: Float
+        get() = if (isOrbit) {
+            solarSettings.orbitSpeedMultiplier
+        } else {
+            solarSettings.rotationSpeedMultiplier
+        }
+
+}
