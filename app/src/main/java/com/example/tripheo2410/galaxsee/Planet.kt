11c4b9e8fc1361402416baa4bb66d240f4218194
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-30 11:41:26 +0300

    fix minor bug

--- app/src/main/java/com/example/tripheo2410/galaxsee/Planet.kt
+++ app/src/main/java/com/example/tripheo2410/galaxsee/Planet.kt
@@ -1,10 +1,20 @@
 package com.example.tripheo2410.galaxsee
 
+import android.content.Context
 import android.view.MotionEvent
+import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.HitTestResult
 import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
 
-class Planet {
+class Planet (
+    private val context: Context,
+    private val planetName: String,
+    private val planetScale: Float,
+    private val planetRenderable: ModelRenderable,
+    private val solarSettings: SolarSettings) : Node(), Node.OnTapListener {
     private var infoCard: Node? = null
     companion object {
 
@@ -12,6 +22,25 @@ class Planet {
 
         init {
             setOnTapListener(this)
+        }
+
+        override fun onUpdate(frameTime: FrameTime?) {
+            if (infoCard == null) {
+                return
+            }
+
+            // Typically, getScene() will never return null because onUpdate() is only called when the node
+            // is in the scene.
+            // However, if onUpdate is called explicitly or if the node is removed from the scene on a
+            // different thread during onUpdate, then getScene may be null.
+            if (scene == null) {
+                return
+            }
+            val cameraPosition = scene.camera.worldPosition
+            val cardPosition = infoCard!!.worldPosition
+            val direction = Vector3.subtract(cameraPosition, cardPosition)
+            val lookRotation = Quaternion.lookRotation(direction, Vector3.up())
+            infoCard!!.worldRotation = lookRotation
         }
 
         override fun onTap(hitTestResult: HitTestResult?, motionEvent: MotionEvent?) {
