44ad3bfd3a9965afa6babefab64ca257035a796e
Minh Tri Hoang <tripheo0412@gmail.com>
2018-09-30 11:38:55 +0300

    add onTap()

--- app/src/main/java/com/example/tripheo2410/galaxsee/Planet.kt
+++ app/src/main/java/com/example/tripheo2410/galaxsee/Planet.kt
@@ -1,2 +1,21 @@
 package com.example.tripheo2410.galaxsee
 
+import android.view.MotionEvent
+import com.google.ar.sceneform.HitTestResult
+import com.google.ar.sceneform.Node
+
+class Planet {
+    private var infoCard: Node? = null
+    companion object {
+
+        private val INFO_CARD_Y_POS_COEFF = 0.55f
+
+        override fun onTap(hitTestResult: HitTestResult?, motionEvent: MotionEvent?) {
+            if (infoCard == null) {
+                return
+            }
+
+            infoCard!!.isEnabled = !infoCard!!.isEnabled
+        }
+    }
+}
