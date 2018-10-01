91e92e4aad86b3dc2ad0d4acd7a93dad06a65540
tripheo0412 <tripheo0412@gmail.com>
2018-10-01 09:52:35 +0300

    add create planet func

--- app/src/main/java/com/example/tripheo2410/galaxsee/MainActivity.kt
+++ app/src/main/java/com/example/tripheo2410/galaxsee/MainActivity.kt
@@ -292,4 +292,26 @@ class MainActivity : AppCompatActivity() {
 
         return base
     }
+
+    private fun createPlanet(
+            name: String,
+            parent: Node,
+            auFromParent: Float,
+            orbitDegreesPerSecond: Float,
+            renderable: ModelRenderable?,
+            planetScale: Float): Node {
+        // Orbit is a rotating node with no renderable positioned at the sun.
+        // The planet is positioned relative to the orbit so that it appears to rotate around the sun.
+        // This is done instead of making the sun rotate so each planet can orbit at its own speed.
+        val orbit = RotatingNode(solarSettings, true)
+        orbit.setDegreesPerSecond(orbitDegreesPerSecond)
+        orbit.setParent(parent)
+
+        // Create the planet and position it relative to the sun.
+        val planet = Planet(this, name, planetScale, renderable!!, solarSettings)
+        planet.setParent(orbit)
+        planet.localPosition = Vector3(auFromParent * AU_TO_METERS, 0.0f, 0.0f)
+
+        return planet
+    }
 }
