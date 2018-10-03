1ece71b533a0c6ffdac2967b7599c79ae4a57fc0
Minh Tri Hoang <tripheo0412@gmail.com>
2018-10-03 17:02:56 +0300

    finish splash screen

--- app/src/main/java/com/example/tripheo2410/galaxsee/SplashScreenActivity.kt
+++ app/src/main/java/com/example/tripheo2410/galaxsee/SplashScreenActivity.kt
@@ -3,6 +3,9 @@ package com.example.tripheo2410.galaxsee
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
+
+
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -10,9 +13,7 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val intent = Intent(applicationContext,
-                MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        val myIntent = Intent(this, MainActivity::class.java)
+        this.startActivity(myIntent)
     }
 }
